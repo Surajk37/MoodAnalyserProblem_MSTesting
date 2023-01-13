f183b7c7e23a5267a0deef5866cac71c23142043
Surajk37 <surajkhot9993@gmail.com>
2023-01-13 20:46:48 +0530

    Welcome Message Added

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome To Mood Analyser Problem using MSTest\n");
+            Console.ReadLine();
+        }
+    }
+}
