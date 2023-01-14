33afb50228fe88e240fd9d8cd81482e27c212b26
Surajk37 <surajkhot9993@gmail.com>
2023-01-14 21:39:30 +0530

    To analyse and respond Happy or Sad Mood

--- MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome To Mood Analyser Problem using MSTest\n");
+            
+            //UC1:-Respond Happy and Sad Mood
+            MoodAnalyzer analyser = new MoodAnalyzer("I am in Sad Mood");
+            Console.WriteLine("I am in Sad Mood:- " + analyser.AnalyzeMood());
+            MoodAnalyzer analysermood = new MoodAnalyzer("I am in Happy Mood");
+            Console.WriteLine("I am in Any Mood:- " + analysermood.AnalyzeMood());
+
             Console.ReadLine();
         }
     }
